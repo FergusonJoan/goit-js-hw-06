2351d4cb788df80fd502e6b7e276639411286473
Joan <prosandcons500@gmail.com>
2023-01-23 21:45:50 +0200

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -6,48 +6,48 @@
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 //? Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
-// const formEl = document.querySelector(".login-form");
-
-// const buttonEl = document.querySelector(".login-form button");
-// const elementsEl = formEl.elements;
-// const inputsEl = document.querySelectorAll(".login-form input");
-
-// console.log(inputsEl);
-
-// formEl.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const formData = new FormData(formEl);
-//   const obj = {};
-
-//   formData.forEach((value, key) => {
-//     if (!value) {
-//       return alert("Bсе поля должны быть заполнены");
-//     }
-//     obj[key] = value;
-//     event.currentTarget.reset();
-//   });
-//   console.log(obj);
-// });
-
 const formEl = document.querySelector(".login-form");
 
 const buttonEl = document.querySelector(".login-form button");
 const elementsEl = formEl.elements;
 const inputsEl = document.querySelectorAll(".login-form input");
 
+console.log(inputsEl);
+
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formData = new FormData(formEl);
+  const obj = {};
 
-  const formData = [...event.currentTarget.formEl].reduce(
-    (formData, { element, value }) => {
-      if (!value) {
-        return alert("Bсе поля должны быть заполнены");
-      }
-      element = value;
-      event.currentTarget.reset();
-      return formData;
-    },
-    {}
-  );
-  console.log(formData);
+  formData.forEach((value, key) => {
+    if (!value) {
+      return alert("Bсе поля должны быть заполнены");
+    }
+    obj[key] = value;
+    event.currentTarget.reset();
+  });
+  console.log(obj);
 });
+
+// const formEl = document.querySelector(".login-form");
+
+// const buttonEl = document.querySelector(".login-form button");
+// const elementsEl = formEl.elements;
+// const inputsEl = document.querySelectorAll(".login-form input");
+
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const formData = [...event.currentTarget.formEl].reduce(
+//     (formData, { element, value }) => {
+//       if (!value) {
+//         return alert("Bсе поля должны быть заполнены");
+//       }
+//       element = value;
+//       event.currentTarget.reset();
+//       return formData;
+//     },
+//     {}
+//   );
+//   console.log(formData);
+// });
